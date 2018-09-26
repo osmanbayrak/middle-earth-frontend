670cf7bdc5e00971fe0a8c104fadde9b5052af7a
osman <osman.bayrak@sekomyazilim.com.tr>
2018-09-26 11:59:19 +0300

    Troop training animations

--- app/home/home.js
+++ app/home/home.js
@@ -22,44 +22,45 @@ angular.module('myApp.home', ['ngRoute', 'ui.bootstrap'])
         return Math.floor(data);
     };
 
-    $scope.page = function () {
-        if (localStorage.getItem('profile') !== undefined) {
-
-            $http.get("http://127.0.0.1:8000/profiles/?user__username=" + JSON.parse(localStorage.getItem('profile')).user.username)
-                .success(function (profile) {
-                    $scope.profile = profile[0];
-                    $scope.currentTown = $scope.profile.town[0];
-                    $scope.allTowns = [];
-                    angular.forEach($scope.profile.town, function (v) {
-                        $scope.allTowns.push(v);});
-                    $scope.zones = {north: {lancers:[], cavalry:[], archers: []},
-                                    south: {lancers:[], cavalry:[], archers: []},
-                                    west: {lancers:[], cavalry:[], archers: []},
-                                    east: {lancers:[], cavalry:[], archers: []},
-                                    center: {lancers:[], cavalry:[], archers: []}};
-                    angular.forEach($scope.currentTown.troops, function (v) {
-                        if (v.type === "lancer") {
-                            $scope.zones[v.town_position].lancers.push(v);
-                        } else if (v.type === "cavalry") {
-                            $scope.zones[v.town_position].cavalry.push(v);
-                        } else if (v.type === "archer") {
-                            $scope.zones[v.town_position].archers.push(v);
-                        }
-                    });
-                    console.log($scope.zones);
-                    angular.forEach($scope.currentTown.buildings, function (v) {
-                        $scope[v.type] = v;
-                        if (v.status === 'loading') {
-                            $scope.buildingLoading(v);
-                        }
-                    });
-                    $scope.currentTown.resources = JSON.parse($scope.currentTown.resources.replace(/u/g,'').replace(/\'/g,'\"'));
-                })
-                .error(function (data, status, header, config) {$location.path('/register');});
-
-        } else { $location.path('/register'); }
+    $scope.page = function (willModalOpen, building) {
+        $http.get("http://127.0.0.1:8000/profiles/?user__username=" + JSON.parse(localStorage.getItem('profile')).user.username)
+            .success(function (profile) {
+                $scope.profile = profile[0];
+                $scope.currentTown = $scope.profile.town[0];
+                $scope.allTowns = [];
+                angular.forEach($scope.profile.town, function (v) {
+                    $scope.allTowns.push(v);});
+                $scope.zones = {north: {lancers:[], cavalry:[], archers: []},
+                                south: {lancers:[], cavalry:[], archers: []},
+                                west: {lancers:[], cavalry:[], archers: []},
+                                east: {lancers:[], cavalry:[], archers: []},
+                                center: {lancers:[], cavalry:[], archers: []}};
+                angular.forEach($scope.currentTown.troops, function (v) {
+                    if (v.type === "lancer") {
+                        $scope.zones[v.town_position].lancers.push(v);
+                    } else if (v.type === "cavalry") {
+                        $scope.zones[v.town_position].cavalry.push(v);
+                    } else if (v.type === "archer") {
+                        $scope.zones[v.town_position].archers.push(v);
+                    }
+                });
+                angular.forEach($scope.currentTown.buildings, function (v) {
+                    $scope[v.type] = v;
+                    if (v.status === 'loading') {
+                        $scope.buildingLoading(v);
+                    }
+                });
+                $scope.currentTown.resources = JSON.parse($scope.currentTown.resources.replace(/u/g,'').replace(/\'/g,'\"'));
+                if (willModalOpen === true) {
+                    $scope.buildingModal(building);
+                }
+                if (("modalReOpen" in $rootScope)? $rootScope.modalReOpen.status: false) {
+                    $scope.buildingModal($rootScope.modalReOpen.building);
+                }
+            })
+            .error(function (data, status, header, config) {window.alert(data);});
     };
-    $scope.refresh = setInterval($scope.page, 120000);
+    var refresh = setInterval($scope.page, 60000);
 
     $scope.buildingLoading = function (building) {
         if (!(building.type in $scope.interval)) {
@@ -68,8 +69,7 @@ angular.module('myApp.home', ['ngRoute', 'ui.bootstrap'])
             $scope.interval[building.type] = setInterval(frame, 1000);
         }
         function frame() {
-            if (sofar >= building.construction_time + 2) {
-                console.log($scope.interval);
+            if (sofar >= building.construction_time + 3) {
                 clearInterval($scope.interval[building.type]);
                 delete $scope.interval[building.type];
                 a.style.width = '0%';
@@ -78,51 +78,75 @@ angular.module('myApp.home', ['ngRoute', 'ui.bootstrap'])
             } else {
                 sofar++;
                 if (building.construction_time >= sofar) {
-                    a.style.width = sofar * 100 / building.construction_time + '%';
+                    a.style.width = sofar *  100 / building.construction_time + '%';
                     a.innerHTML = $scope.convertSeconds(building.construction_time - sofar);
                 } else {a.innerHTML = "Loading!";}
-            }}};
+            }
+        }
+    };
 
     $rootScope.buildingProcess = function (building, isCancel) {
         if (isCancel === false) {
             building.status = "loading";
             building.change_date = new Date();
-        } else {building.status = "completed";}
-        $http.put("http://127.0.0.1:8000/buildings/" + building.id + '/', building)
-            .success(function (res) {
-                if (res.status === "loading") {
-                     $http.put("http://127.0.0.1:8000/towns/"+res.town+"/", {resources:"{'wood':"+(parseInt($scope.currentTown.resources.wood) - (building.cost["wood"] ? parseInt(building.cost["wood"]):0)) + ", 'food':"+(parseInt($scope.currentTown.resources.food) - (building.cost["food"] ? parseInt(building.cost["food"]):0)) +", 'stone':" + (parseInt($scope.currentTown.resources.stone) - (building.cost["stone"] ? parseInt(building.cost["stone"]):0)) +"}"})
-                         .success(function (res) {
-                             $scope.page();
-                         })
-                         .error(function (err, status, header, config) {console.log("put da error var");});
-                } else {
-                    $http.put("http://127.0.0.1:8000/towns/"+res.town+"/", {resources:"{'wood':"+(parseInt($scope.currentTown.resources.wood) + (building.cost["wood"] ? parseInt(building.cost["wood"]):0)) + ", 'food':"+(parseInt($scope.currentTown.resources.food) + (building.cost["food"] ? parseInt(building.cost["food"]):0)) +", 'stone':" + (parseInt($scope.currentTown.resources.stone) + (building.cost["stone"] ? parseInt(building.cost["stone"]):0)) +"}"})
-                        .success(function (res) {
-                            clearInterval($scope.interval[building.type]); delete $scope.interval[building.type]; $scope.page();
-                        })
-                        .error(function (err, status, header, config) {console.log("put da error var");});
-                }
-            })
-            .error(function (err, status, header, config) {window.alert("You have not enough resources!"); $scope.page();});
-    };
 
-    $rootScope.trainNew = function (buildingType, isCancel) {
-        if (isCancel === false) {
-            if (buildingType === 'archery') {
-                $scope.troop = {type:'archer', tier:0, town:$scope.currentTown.id, change_date: new Date(), status: 'preparing', town_position:'center'};
-            } else if(buildingType === 'stable') {
-                $scope.troop = {type:'cavalry', tier:0, town:$scope.currentTown.id, change_date: new Date(), status: 'preparing', town_position:'center'};
-            } else if(buildingType === 'barrack') {
-                $scope.troop = {type:'lancer', tier:0, town:$scope.currentTown.id, change_date: new Date(), status: 'preparing', town_position:'center'};
-            }
-            $http.post("http://127.0.0.1:8000/buildings/", $scope.troop)
+            $http.put("http://127.0.0.1:8000/buildings/" + building.id + '/', building)
                 .success(function (res) {
                     $scope.page();
                 })
+                .error(function (err, status, header, config) {window.alert("You have not enough resources!"); $scope.page();});
+        } else {
+            building.status = "completed";
+            $http.put("http://127.0.0.1:8000/buildings/" + building.id + '/', building)
+                .success(function (res) {
+                    clearInterval($scope.interval[building.type]);
+                    delete $scope.interval[building.type];
+                    $scope.page();
+                })
                 .error(function (err, status, header, config) {window.alert("Check your queue and resources and try again!"); $scope.page();});
+        }
+    };
 
-        } else { var del ="delete created object from db";}
+    $rootScope.trainNew = function (initial_troop, isCancel, troop_id) {
+        if (isCancel === false) {
+            if (initial_troop.type === 'archer') {
+                $scope.troop = {type:'archer', tier:0, town:$scope.currentTown.id, change_date: new Date(), status: 'preparing', town_position:'center'};
+            } else if(initial_troop.type === 'cavalry') {
+                $scope.troop = {type:'cavalry', tier:0, town:$scope.currentTown.id, change_date: new Date(), status: 'preparing', town_position:'center'};
+            } else if(initial_troop.type === 'lancer') {
+                $scope.troop = {type:'lancer', tier:0, town:$scope.currentTown.id, change_date: new Date(), status: 'preparing', town_position:'center'};
+            }
+            $http.post("http://127.0.0.1:8000/troops/", $scope.troop)
+                .success(function (res) {
+                    $scope.page();
+                })
+                .error(function (err, status, header, config) {window.alert("Check your queue and resources then try again!"); $scope.page();});
+
+        } else {
+            $http.delete("http://127.0.0.1:8000/troops/"+troop_id+"/")
+                .success(function (res) {
+                    var cancelData = {
+                        resources:JSON.stringify({
+                            "wood":$scope.currentTown.resources.wood + ("wood" in initial_troop.cost ? initial_troop.cost.wood:0),
+                            "food":$scope.currentTown.resources.food + ("food" in initial_troop.cost ? initial_troop.cost.food:0),
+                            "stone":$scope.currentTown.resources.stone + ("stone" in initial_troop.cost ? initial_troop.cost.stone:0)
+                        }),
+                        troop_queue:$scope.currentTown.troop_queue - 1
+                    };
+                    $http.put("http://127.0.0.1:8000/towns/"+ $scope.currentTown.id +"/", cancelData)
+                        .success(function (res) {
+                            //clearInterval(interval.id.oftroop);
+                            //delete $scope.interval[interval.id.oftroop];
+                            $scope.page();
+                        })
+                        .error(function (err, status) {
+                            window.alert("Cant REPAY you because of" + err);
+                        });
+                })
+                .error(function (err,status) {
+                    window.alert("Cant cancel because of" + err);
+                });
+        }
     };
 
     $scope.page();
--- app/modals/building/building.js
+++ app/modals/building/building.js
@@ -3,44 +3,89 @@
 angular.module('myApp.buildingModal', ['ngRoute', 'ui.bootstrap'])
 
 .controller('buildingModalCtrl', ['$scope','$rootScope', '$http', '$location','$uibModal', '$uibModalInstance', 'modalConfig', function($scope, $rootScope, $http, $location, $uibModal, $uibModalInstance, modalConfig) {
-    $scope.building = modalConfig.building;
-    $scope.town = modalConfig.town;
-    $scope.resources = $scope.town.resources;
-    $scope.troops = $scope.town.troops;
-    $scope.preparing_troops = [];
-    angular.forEach($scope.troops, function (v) {
-        if (v.status === 'preparing') {
-            $scope.preparing_troops.push(v);
-        }
-    });
-    $scope.upB = function () {
-        $rootScope.buildingProcess($scope.building, false); // this function is in home.js and second parameter is for isCancel
-        $uibModalInstance.close();
-    };
-    $scope.cancel = function () {
-        $rootScope.buildingProcess($scope.building, true);
-        $uibModalInstance.close();
-    };
-
-    $scope.train = function () {
-        $rootScope.trainNew($scope.building.type, false); // trainProcess(buildingTypeForTrainType, isCancel)
-        $uibModalInstance.close();
-    };
+    $scope.interval = {};
     $scope.convertSeconds = function (seconds) {
         var date = new Date(null);
         date.setSeconds(seconds);
         return date.toISOString().substr(11, 8);
     };
 
+    $scope.building = modalConfig.building;
+    $rootScope.modalReOpen = {status:false, building: $scope.building};
+    $scope.town = modalConfig.town;
+    $scope.resources = $scope.town.resources;
+    $scope.troops = $scope.town.troops;
+    $scope.preparing_troops = [];
+
+    $scope.troopPreparing = function (troop) {
+        if (!(troop.id in $scope.interval)) {
+            var a = document.getElementById(troop.id +'Loading');
+            var sofar = parseInt((new Date().getTime() - new Date(troop.change_date).getTime())/1000);
+            $scope.interval[troop.id] = setInterval(frame, 1000);
+        }
+        function frame() {
+            if (sofar >= troop.preparation_time + 4) {
+                clearInterval($scope.interval[troop.id]);
+                delete $scope.interval[troop.id];
+                a.style.width = '0%';
+                a.innerHTML = "Initilazing";
+                $rootScope.modalReOpen.status = true;
+                $uibModalInstance.close();
+            } else {
+                sofar++;
+                if (troop.preparation_time >= sofar) {
+                    a.style.width = sofar *  100 / troop.preparation_time + '%';
+                    a.innerHTML = $scope.convertSeconds(troop.preparation_time - sofar);
+                } else {a.innerHTML = "Loading!";}
+            }
+        }
+    };
+
+    angular.forEach($scope.troops, function (v) {
+        if (v.status === "preparing") {
+            if ($scope.building.type === "archery" && v.type === 'archer') {
+                $scope.preparing_troops.push(v);
+                setTimeout(function() {$scope.troopPreparing(v);}, 250);
+            }
+            if ($scope.building.type === "stable" && v.type === 'cavalry') {
+                $scope.preparing_troops.push(v);
+                setTimeout(function() {$scope.troopPreparing(v);}, 250);
+            }
+            if ($scope.building.type === "lancer" && v.type === 'lancer') {
+                $scope.preparing_troops.push(v);
+                setTimeout(function() {$scope.troopPreparing(v);}, 250);
+            }
+        }
+    });
+
+    $scope.upBuilding = function () {
+        $rootScope.buildingProcess($scope.building, false); // this function is in home.js and second parameter is for isCancel
+        $uibModalInstance.close();
+    };
+    $scope.cancelBuilding = function () {
+        $rootScope.buildingProcess($scope.building, true);
+        $uibModalInstance.close();
+    };
 
     if ($scope.building.type === "archery") {
-        $scope.initial_troop = {cost: {food: 900, wood: 400, stone: 300}, preparation_time: 480, img: "images/military/archer/profile.png"};
+        $scope.initial_troop = {type: 'archer', cost: {food: 900, wood: 400, stone: 300}, preparation_time: 480, img: "images/military/archer/profile.png"};
     } else if ($scope.building.type === "barrack") {
-        $scope.initial_troop = {cost: {food: 800, wood: 300, stone: 250}, preparation_time: 360, img: "images/military/lancer/profile.png"};
+        $scope.initial_troop = {type: 'lancer', cost: {food: 800, wood: 300, stone: 250}, preparation_time: 360, img: "images/military/lancer/profile.png"};
     } else if ($scope.building.type === "stable") {
-        $scope.initial_troop = {cost: {food: 2500, wood: 650, stone: 500}, preparation_time: 840, img: "images/military/cavalry/profile.png"};
+        $scope.initial_troop = {type: 'cavalry', cost: {food: 2500, wood: 650, stone: 500}, preparation_time: 840, img: "images/military/cavalry/profile.png"};
     }
 
+    $scope.train = function () {
+        $rootScope.trainNew($scope.initial_troop, false, undefined); // trainProcess(buildingTypeForTrainType, isCancel, idOfTRoop)
+        $rootScope.modalReOpen.status = true;
+        $uibModalInstance.close();
+    };
+
+    $scope.cancel_train = function (troop_id) {
+        $rootScope.trainNew($scope.initial_troop, true, troop_id); // trainProcess(troopType, isCancel, idOfTroop)
+        $rootScope.modalOpen.status = true;
+        $uibModalInstance.close();
+    };
 
     $scope.checkEnough = function (resourcesType, cost) {
         if (cost !== undefined ? ($scope.resources[resourcesType] >= cost):true) {
